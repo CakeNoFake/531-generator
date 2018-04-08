be44d389f255ca55ecfa60791a761795cea378a4
CakeNoFake <37895456+CakeNoFake@users.noreply.github.com>
2018-04-08 04:23:29 -0400

    90 -> 95; harder TM

--- gym.js
+++ gym.js
@@ -7,11 +7,11 @@ var BarbellRow;
 function showProgram() {
 
     //get the lifts
-    Bench = document.getElementById('bench').value * 0.9;
-    Squat = document.getElementById('squat').value * 0.9;
-    DeadLift = document.getElementById('deadlift').value * 0.9;
-    OverheadPress = document.getElementById('overheadpress').value * 0.9;
-    BarbellRow = document.getElementById('barbellrow').value * 0.9;
+    Bench = document.getElementById('bench').value * 0.95;
+    Squat = document.getElementById('squat').value * 0.95;
+    DeadLift = document.getElementById('deadlift').value * 0.95;
+    OverheadPress = document.getElementById('overheadpress').value * 0.95;
+    BarbellRow = document.getElementById('barbellrow').value * 0.95;
 
     localStorage["Bench"] = Bench;
     localStorage["Squat"] = Squat;
