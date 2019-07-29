694a62800fe533b3ea16842c92eea98650fbbaeb
CakeNoFake <37895456+CakeNoFake@users.noreply.github.com>
2019-07-29 06:59:07 -0400

    2x warmup instead of 3

--- program/program.js
+++ program/program.js
@@ -63,7 +63,7 @@ function fillData() {
     var warmupArr = document.getElementsByClassName("warmup");
 
     for(var i = 0; i < warmupArr.length; i++){
-        document.getElementsByClassName("warmup")[i].innerHTML = "3min - <br>Treadmill" + "<br>" + "3x15 - <br>Box jumps";
+        document.getElementsByClassName("warmup")[i].innerHTML = "2min - <br>Treadmill" + "<br>" + "2x15 - <br>Box jumps";
     }    
 
     //fill Lifts
